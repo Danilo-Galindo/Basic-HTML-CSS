b9fd393ee6ff4fd8e5e148c86deb94e3e6c0b9e4
Danilo <danilog2323@gmail.com>
2020-10-15 09:42:31 -0500

    Creating function to show number greater than 1

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log("Hello my friend!!!")
+let arr =[0,1,2,3,4,5,6,7]
+
+function run(arr){
+    for(let i of arr){
+        if(arr[i]>1){
+            console.log(arr[i])
+        }
+    }
+}
+
+run(arr)
